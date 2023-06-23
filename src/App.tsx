6e8b33fb0472c730cf2ce52a4dfb69826b1fd9fa
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-06-24 00:44:45 +0530

    routr dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MantineProvider, Text } from "@mantine/core";
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { Home } from './pages'
+import { Login } from "./pages/Login/Login"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<h1>Hello</h1>}/>
+              <Route path="/auth" element={<Login/>}/>
+            </Routes>
+          </BrowserRouter>
+      </MantineProvider>
     </div>
   );
 }
